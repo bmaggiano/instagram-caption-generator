7726282b2e5d1e52adff7f2fa4ba8c398e692618
bmaggiano <brandon.maggiano@gmail.com>
2023-04-20 12:00:28 -0700

    added file structure for /instagram and /instagram/caption

---
+++ pages/instagram/caption.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Caption() {
+  return (
+    <div>
+      <h1>caption</h1>
+    </div>
+  )
+}
+
+export default Caption
---
+++ pages/instagram/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Instagram() {
+  return (
+    <div>
+      <h1>Instagram</h1>
+    </div>
+  )
+}
+
+export default Instagram
