7aa3ef01418c4328f04544ef7dcae4cd81743058
bmaggiano <brandon.maggiano@gmail.com>
2023-05-09 15:45:57 -0700

    clean up some unused code in index.tsx

--- pages/test.tsx
+++ pages/test.tsx
@@ -24,41 +24,39 @@ export default function MultiUploader() {
 
   return (
     <>
-    <div
-      {...getRootProps()}
-      className="mt-3 flex items-center justify-center w-full"
-    >
-      <label
-        htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+      <div
+        {...getRootProps()}
+        className="mt-3 flex items-center justify-center w-full"
       >
-        <input {...getInputProps()} id="dropzone-file" type="file" />
-        Drop files here!
-        {imageUrl && (
-          <img
-            src={imageUrl}
-            className="h-2/3 mb-3 text-gray-400"
-            alt="uploaded image"
-          />
-        )}
-      </label>
-    </div>
-    <div className="mt-3 flex flex-col items-center justify-center w-full">
-
-          <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-50"
-
-            onClick={async () => {
-              const metadata = await startUpload();
-              const imageUrl = metadata[0]?.fileUrl;
-              localStorage.setItem("imageUrl", imageUrl);
-              setImageUrl(imageUrl);
-              window.location.reload();
-            }}
-          >
-            Upload {files.length} files
-          </button>
-          </div>
+        <label
+          htmlFor="dropzone-file"
+          className="flex flex-col items-center justify-center w-full h-96 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        >
+          <input {...getInputProps()} id="dropzone-file" type="file" />
+          Add pictures here!
+          {imageUrl && (
+            <img
+              src={imageUrl}
+              className="h-2/3 mb-3 text-gray-400"
+              alt="uploaded image"
+            />
+          )}
+        </label>
+      </div>
+      <div className="mt-3 flex flex-col items-center justify-center w-full">
+        <button
+          className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-50"
+          onClick={async () => {
+            const metadata = await startUpload();
+            const imageUrl = metadata[0]?.fileUrl;
+            localStorage.setItem("imageUrl", imageUrl);
+            setImageUrl(imageUrl);
+            window.location.reload();
+          }}
+        >
+          Upload & Generate Caption
+        </button>
+      </div>
     </>
   );
 }
