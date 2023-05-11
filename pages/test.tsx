8fc942d753b7ce8b16e60710221ea855e89bb6a3
bmaggiano <brandon.maggiano@gmail.com>
2023-05-11 12:23:25 -0700

    added loading dots to uploading image and completed logic for handling image upload (last element in array)

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,18 +1,21 @@
 import { generateReactHelpers } from "@uploadthing/react";
 import type { OurFileRouter } from "../server/uploadthing";
 import { useState, useEffect } from "react";
+import LoadingDots from "../components/LoadingDots";
 
 const { useUploadThing } = generateReactHelpers<OurFileRouter>();
 
 export default function MultiUploader() {
   const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const { getRootProps, getInputProps, isDragActive, files, startUpload } =
     useUploadThing("imageUploader");
 
-  console.log(files)
-
-  const userImage = files.length > 0 && files[files.length - 1]?.contents ? files[files.length - 1]?.contents : null;
+  const userImage =
+    files.length > 0 && files[files.length - 1]?.contents
+      ? files[files.length - 1]?.contents
+      : null;
 
   useEffect(() => {
     const imageUrlFromStorage = localStorage.getItem("imageUrl");
@@ -57,25 +60,29 @@ export default function MultiUploader() {
         </label>
       </div>
 
-{files.length > 0 && (
-      <div className="mt-3 flex flex-col items-center justify-center w-full">
-        <button
-          className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-50"
-          onClick={async () => {
-            const metadata = await startUpload();
-            const lastMetadata = metadata[metadata.length - 1];
-            const imageUrl = lastMetadata?.fileUrl;
-                        localStorage.setItem("imageUrl", imageUrl);
-            setImageUrl(imageUrl);
-            window.location.reload();
-          }}
-        >
-          Upload {files.length} files
-        </button>
-      </div>
-)}
-
-
+      {files.length > 0 && (
+        <div className="mt-3 flex flex-col items-center justify-center w-full">
+{ !loading &&         <button
+            className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-50"
+            onClick={async () => {
+              setLoading(true)
+              const metadata = await startUpload();
+              const lastMetadata = metadata[metadata.length - 1];
+              const imageUrl = lastMetadata?.fileUrl;
+              localStorage.setItem("imageUrl", imageUrl);
+              setImageUrl(imageUrl);
+              window.location.reload();
+            }}
+          >
+            Upload image
+          </button>}
+          {loading && (
+            <button className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-50">
+              <LoadingDots color="white" style="large"/>
+            </button>
+          )}
+        </div>
+      )}
     </>
   );
 }
