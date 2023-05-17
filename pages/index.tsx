a1838e4095af7bd95583b412e91d388cb1bebcb9
bmaggiano <brandon.maggiano@gmail.com>
2023-05-17 14:32:46 -0700

    changed input accept to allow android users to input images too

--- pages/index.tsx
+++ pages/index.tsx
@@ -253,7 +253,7 @@ function Instagram() {
           htmlFor="dropzone-file"
           className="flex flex-col items-center justify-center w-full h-72 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
         >
-          <input {...getInputProps()} id="dropzone-file" type="file" accept="image/png, image/jpeg, image/heic" />
+          <input {...getInputProps()} id="dropzone-file" type="file" accept="image/png, image/jpeg, image/webp, image/apng, image/bmp, image/x-icon" />
           {!userImage && !imageUrl && <>Upload Picture Here</>}{" "}
           {userImage && imageUrl && (
             <img
